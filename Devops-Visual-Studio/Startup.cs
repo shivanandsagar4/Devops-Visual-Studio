df0a776ff62df4fcde0b6adfa2af5f87f30f41c2
shivanandsagar4 <shivanandsagar4@gmail.com>
2021-03-11 20:48:56 +0530

    Pushing from local repo

--- Devops-Visual-Studio/Startup.cs
+++ Devops-Visual-Studio/Startup.cs
@@ -15,7 +15,7 @@ namespace Devops_Visual_Studio
     {
         public Startup(IConfiguration configuration)
         {
-            //Change Made + From Local Repo
+            //Change Made + From Local Repo1
             Configuration = configuration;
         }
 
